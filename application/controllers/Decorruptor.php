cbc7ba322300aff8db691b6362e84868d77d22be
JuanIrizo <juanirizo@hotmail.com>
2016-09-28 20:48:07 +0200

    Proyecto Decorruptor en CodeIgniter, y ya he elegido la interfaz

---
+++ application/controllers/Decorruptor.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Decorruptor extends CI_Controller {
+
+
+	public function index()
+	{
+		$this->load->view('Decorruptor_view');
+	}
+}
